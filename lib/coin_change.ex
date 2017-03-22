d002328f500c00ff98449518611b82cf0f8af32a
Hugo Roque <hugolnx@gmail.com>
2017-03-22 19:58:22 -0300

    starting coinchange problem

---
+++ lib/coin_change.ex
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/ctci-coin-change
+defmodule CoinChange do
+  def solve() do
+  end
+end
---
+++ test/coin_change_test.exs
@@ -0,0 +1,7 @@
+defmodule CoinChangeTeste do
+  use ExUnit.Case
+
+  test "works" do
+    assert true
+  end
+end
