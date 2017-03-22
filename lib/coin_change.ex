56ad963ec2736916570173538b951f3c52370b3c
Hugo Roque <hugolnx@gmail.com>
2017-03-22 20:12:15 -0300

    First implementation draft of coin change

--- lib/coin_change.ex
+++ lib/coin_change.ex
@@ -1,5 +1,11 @@
 # https://www.hackerrank.com/challenges/ctci-coin-change
 defmodule CoinChange do
-  def solve() do
+  def solve(amount, coins) do
+    cond do
+      amount == 0 -> 1
+      amount < 0 -> 0
+      _ -> 
+        
+    end
   end
 end
--- test/coin_change_test.exs
+++ test/coin_change_test.exs
@@ -1,7 +1,15 @@
 defmodule CoinChangeTeste do
   use ExUnit.Case
 
-  test "works" do
-    assert true
+  test "return 0 if only have bigger coins than the value" do
+    assert CoinChange.solve(3, [5, 6, 7]) == 0
+  end
+
+  test "return 0 if can not combine the coins to get the change" do
+    assert CoinChange.solve(3, [2]) == 0
+  end
+
+  test "return 1 if there is only one combination of coins to get the change" do
+    assert CoinChange.solve(6, [5,1]) == 1
   end
 end
